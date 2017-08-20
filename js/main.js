72cb213fa8c0465a5f994b88f0985eb933377d17
Chia Amisola <chiamisola@gmail.com>
2017-08-20 23:18:06 +0800

    Keith please don't get mad at me

---
+++ js/main.js
@@ -0,0 +1,18 @@
+  $(function(){
+      $(".typedTestimonials").typed({
+     strings: ["Chia, go to sleep na.<br /><em>- Mom</em>", "How's my big boy!<br /><em>- Keith's Mom</em>"],
+		typeSpeed: 20,
+			// time before typing starts
+			startDelay: 100,
+			// backspacing speed
+			backSpeed: 50,
+			// time before backspacing
+			backDelay: 2500,
+			// loop
+			loop: true,
+			// false = infinite
+			loopCount: false,
+			// show cursor
+			showCursor: true,
+      });
+  });

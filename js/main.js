98b407881a4f506431bdbeb432fcb2aaac980122
Chia Amisola <chiamisola@gmail.com>
2017-08-21 00:05:50 +0800

    Comic Sans hamburger :)

--- js/main.js
+++ js/main.js
@@ -16,3 +16,32 @@
 			showCursor: true,
       });
   });
+
+
+$(function () {
+    $('a').click(function () {
+        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+            if (target.length) {
+                $('html,body,.container').animate({ scrollTop: target.offset().top }, 1000);
+                return false;
+            }
+        }
+    });
+});
+
+$(document).ready(function() {
+    $('.navigation').click(function(e) {  
+       $('.navigation').fadeOut();
+    });
+    
+    $('.navigation a').click(function(e) {  
+       $('.navigation').fadeOut();
+    });
+    
+});
+
+function hamburger(){
+    $(".navigation").toggle();
+}
